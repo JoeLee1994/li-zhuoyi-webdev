c4973bbfb2b880a6b8d9d62817792586230216fc
joe <Joe@MacBook-Pro.local>
2017-05-27 18:07:18 -0400

    modify

--- public/assignment/views/User/controllers/login.controller.view.js
+++ public/assignment/views/User/controllers/login.controller.view.js
@@ -6,32 +6,18 @@
         .module('WAM')
         .controller('loginController', loginController);
 
-    function loginController($location) {
+    function loginController($location, userService) {
 
         var model = this;
-
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ];
 
         model.login = login;
 
         function login(username, password) {
-            var found = null;
-            for(var u in users) {
-                var user = users[u];
-                if( user.username === username &&
-                    user.password === password) {
-                    found = user;
-                    break;
-                }
-            }
+
+            var found = userService.findUserByCredentials(username, password);
+
             if(found !== null) {
-                //model.message = "welcome " + username;
-                $location.url('/profile／' + found._id);
+                $location.url('/profile/' + found._id);
             } else {
                 model.message = "sorry, " + username + " not found. please try again!";
             }
