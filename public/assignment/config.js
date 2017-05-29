8f6b7de95ccc7e2d2966c809c3d10c8a12139726
joe <Joe@MacBook-Pro.local>
2017-05-28 23:03:42 -0400

    modification

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -12,62 +12,62 @@
                 templateUrl: 'home.html'
             })
             .when('/login', {
-                templateUrl: 'views/User/templates/login.view.client.html',
+                templateUrl: 'views/user/templates/login.view.client.html',
                 controller: 'LoginController',
                 controllerAs: 'model'
             })
             .when('/register', {
-                templateUrl: 'views/User/templates/register.view.client.html',
+                templateUrl: 'views/user/templates/register.view.client.html',
                 controller: 'RegisterController',
                 controllerAs: 'model'
             })
             .when('/user/:userId', {
-                templateUrl: 'views/User/templates/profile.view.client.html',
+                templateUrl: 'views/user/templates/profile.view.client.html',
                 controller: 'ProfileController',
                 controllerAs: 'model'
             })
             .when('/user/:userId/website', {
-                templateUrl: 'views/Website/templates/website-list.view.client.html',
+                templateUrl: 'views/website/templates/website-list.view.client.html',
                 controller: 'WebsiteListController',
                 controllerAs: 'model'
             })
             .when('/user/:userId/website/new', {
-                templateUrl: 'views/Website/templates/website-new.view.client.html',
+                templateUrl: 'views/website/templates/website-new.view.client.html',
                 controller: 'NewWebsiteController',
                 controllerAs: 'model'
             })
             .when('/user/:userId/website/:websiteId', {
-                templateUrl: 'views/Website/templates/website-edit.view.client.html',
+                templateUrl: 'views/website/templates/website-edit.view.client.html',
                 controller: 'EditWebsiteController',
                 controllerAs: 'model'
             })
             .when('/user/:userId/website/:websiteId/page', {
-                templateUrl: 'views/Pages/templates/page-list.view.client.html',
+                templateUrl: 'views/page/templates/page-list.view.client.html',
                 controller: 'PageListController',
                 controllerAs: 'model'
             })
             .when('/user/:userId/website/:websiteId/page/new', {
-                templateUrl: 'views/Pages/templates/page-new.view.client.html',
+                templateUrl: 'views/page/templates/page-new.view.client.html',
                 controller: 'NewPageController',
                 controllerAs: 'model'
             })
             .when('/user/:userId/website/:websiteId/page/:pageId', {
-                templateUrl: 'views/Pages/templates/page-edit.view.client.html',
+                templateUrl: 'views/page/templates/page-edit.view.client.html',
                 controller: 'EditPageController',
                 controllerAs: 'model'
             })
             .when('/user/:userId/website/:websiteId/page/:pageId/widget', {
-                templateUrl: 'views/Widget/templates/widget-list.view.client.html',
+                templateUrl: 'views/widget/templates/widget-list.view.client.html',
                 controller: 'WidgetListController',
                 controllerAs: 'model'
             })
             .when('/user/:userId/website/:websiteId/page/:pageId/widget/new', {
-                templateUrl: 'views/Widget/templates/widget-chooser.view.client.html',
+                templateUrl: 'views/widget/templates/widget-chooser.view.client.html',
                 controller: 'NewWidgetController',
                 controllerAs: 'model'
             })
             .when('/user/:userId/website/:websiteId/page/:pageId/widget/:widgetId', {
-                templateUrl: 'views/Widget/templates/widget-edit.view.client.html',
+                templateUrl: 'views/widget/templates/widget-edit.view.client.html',
                 controller: 'EditWidgetController',
                 controllerAs: 'model'
             })
